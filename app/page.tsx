a0bbc5a3abf7d3522c2833a91eb9b8a11332bfb5
danielFeliseed <daniel@feliseed.co.jp>
2024-07-25 11:58:57 +0900

    removed files

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -28,9 +27,6 @@ export default function Home() {
           </a>
         </div>
       </div>
-
-      <a href="/about">About page</a>
-      <Link href="/about">About page (Link)</Link>
 
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
